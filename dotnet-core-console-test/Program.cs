51f6747d13de148bdb099a36d1c80d7ebf3df385
Joachim Andersen <joachimdamandersen@gmail.com>
2017-09-12 20:59:56 +0200

    added dotnet core console and mvc applications

---
+++ dotnet-core-console-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_core_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
